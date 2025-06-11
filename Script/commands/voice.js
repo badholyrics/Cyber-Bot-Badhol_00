83e32f02b4d6c6a92622655eda922043cd63c5eb
badholyrics <mbadolislam795@gmail.com>
2025-06-11 12:07:30 +0600

    Create voice.js

---
+++ Script/commands/voice.js
@@ -0,0 +1,69 @@
+const axios = require("axios");
+const fs = require("fs");
+const request = require("request");
+
+const link = [
+ "http://islamic-cyber-chat-uploading-side.onrender.com/uploaded/241f84e5.mp3",
+
+];
+
+module.exports.config = {
+ name: "🕋",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "Islamick Chat",
+ description: "auto reply to salam",
+ commandCategory: "noprefix",
+ usages: "🌺",
+ cooldowns: 5,
+ dependencies: {
+ "request":"",
+ "fs-extra":"",
+ "axios":""
+ }
+};
+
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+ const content = event.body ? event.body : '';
+ const body = content.toLowerCase();
+ if (body.startsWith("🕋")) {
+ const rahad = [
+ "╭•┄┅════❁🌺❁════┅┄•╮\n \n one day insaallah 🥺\n\n╰•┄┅════❁🌺❁════┅┄•╯",
+ "╭•┄┅════❁🌺❁════┅┄•╮\n\n feel 🥺\n\n╰•┄┅════❁🌺❁════┅┄•╯"
+
+ ];
+ const rahad2 = rahad[Math.floor(Math.random() * rahad.length)];
+
+ const callback = () => api.sendMessage({
+ body: `${rahad2}`,
+ attachment: fs.createReadStream(__dirname + "/cache/2024.mp3")
+ }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp3"), event.messageID);
+
+ const requestStream = request(encodeURI(link[Math.floor(Math.random() * link.length)]));
+ requestStream.pipe(fs.createWriteStream(__dirname + "/cache/2024.mp3")).on("close", () => callback());
+ return requestStream;
+ }
+};
+
+module.exports.languages = {
+ "vi": {
+ "on": "Dùng sai cách rồi lêu lêu",
+ "off": "sv ngu, đã bão dùng sai cách",
+ "successText": `🧠`,
+ },
+ "en": {
+ "on": "on",
+ "off": "off",
+ "successText": "success!",
+ }
+};
+
+module.exports.run = async ({ api, event, Threads, getText }) => {
+ const { threadID, messageID } = event;
+ let data = (await Threads.getData(threadID)).data;
+ if (typeof data["🕋"] === "undefined" || data["🕋"]) data["🕋"] = false;
+ else data["🕋"] = true;
+ await Threads.setData(threadID, { data });
+ global.data.threadData.set(threadID, data);
+ api.sendMessage(`${(data["🕋"]) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
